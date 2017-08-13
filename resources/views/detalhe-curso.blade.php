53f2e99aa654ef2992a818e9b992ad0beda9c70b
AlexMCoder <alexmpprog@gmail.com>
2017-08-12 22:49:44 -0300

    view details course

--- resources/views/detalhe-curso.blade.php
+++ resources/views/detalhe-curso.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 	<main class="conteudoPrincipal">
 		<div class="container">
-			<h1 style="font-size: 30px;font-weight: bold;padding: 15px;">Detalhe do Curso {{ $curso->name }}</h1>
-
-			
+			<h1 style="font-size: 30px;font-weight: bold;padding: 15px;">
+				Detalhe do Curso {{ $curso->name }}
+			</h1>
 
 			@foreach ($detalhe as $detalhe)
-						<h2>{{ $detalhe->titulo}}</h2>
+				<div class="row">
+					<div class="col-md-6">
+						{!! $detalhe->link !!}
+					</div>
+					<div class="col-md-6">
+						<h1>{{ $detalhe->titulo }}</h1>
+						<div class="detalhe-descricao">
+							{{ $detalhe->descricao }}
+						</div>
+					</div>
+				</div>
 			@endforeach
 		</div>
 	</main>
