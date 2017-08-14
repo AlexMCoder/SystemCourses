30efab042baa076b17e4444ad8336362151e47d8
alex <alex@alexmpprog@gmail.com>
2017-08-14 09:03:37 -0300

    Modicando os ícones na lista de cursos

--- resources/views/cursos/table.blade.php
+++ resources/views/cursos/table.blade.php
@@ -2,6 +2,7 @@
     <thead>
         <th>Nome</th>
         <th>Ícone</th>
+        <th>Descrição</th>
         <th colspan="3">Ação</th>
     </thead>
     <tbody>
@@ -9,13 +10,14 @@
         <tr>
             <td>{!! $curso->name !!}</td>
             <td><img src="/img/cursos/{!! $curso->file !!}" height="60px"/></td>
+            <td>{!! $curso->descricao !!}</td>
             <td>
                 {!! Form::open(['route' => ['cursos.destroy', $curso->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('cursos.show', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Visualizar Dados'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('cursos.edit', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Editar Curso'><i class="glyphicon glyphicon-edit"></i></a>
-                    <a href="{!! route('enviar.video', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Adicionar vídeo aulas'><i class="glyphicon glyphicon-facetime-video"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Tem certeza que deseja deletar este curso?')"]) !!}
+                    <a href="{!! route('cursos.show', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Visualizar Dados'><i class="glyphicon glyphicon-eye-open"></i> Visualizar Dados</a><br>
+                    <a href="{!! route('cursos.edit', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Editar Curso'><i class="glyphicon glyphicon-edit"></i> Editar Curso</a><br>
+                    <a href="{!! route('enviar.video', [$curso->id]) !!}" class='btn btn-default btn-xs' title='Adicionar vídeo aulas'><i class="glyphicon glyphicon-facetime-video"></i> Adicionar vídeo aulas</a><br>
+                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i> Excluir Curso', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Tem certeza que deseja deletar este curso?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
